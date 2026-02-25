2a11d8153d9271c3f6c33eebf6e69c6f38d87c1a
Vibhav Singh <vibhavrajput2005@gmail.com>
2026-02-26 00:10:22 +0530

    Add Vercel link for cors iussue

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    // "https://your-netlify-project.netlify.app"
+    "https://weather-app-g6yu.vercel.app/"
   ],
   credentials: true
 }));
